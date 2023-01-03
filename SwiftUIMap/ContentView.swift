432d2b6b6ce5e017d89543d6f4919799c52f91f6
AhmedKassem <ahmedkassem996@gmail.com>
2023-01-03 09:44:39 +0200

    Initial Commit

---
+++ SwiftUIMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMap
+//
+//  Created by Ahmed Kasem on 03/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIMap/SwiftUIMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapApp.swift
+//  SwiftUIMap
+//
+//  Created by Ahmed Kasem on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
